5f5d9de48c3d5bd4272d783d3fa6458f0c88a8de
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2024-07-15 10:22:29 +0900

    Add files via upload

---
+++ C#/CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+*/
+
+string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+…or create a new repository on the command line
+echo "# c-practice01" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nishi0000/c-practice01.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/nishi0000/c-practice01.git
+git branch -M main
+git push -u origin main
