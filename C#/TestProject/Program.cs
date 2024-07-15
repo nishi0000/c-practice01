be45ca60040e67fc13e570e4f5aafb254bf43932
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2024-07-16 08:57:53 +0900

    Add files via upload

--- C#/TestProject/Program.cs
+++ C#/TestProject/Program.cs
@@ -1,4 +1,31 @@
-﻿// string value1 = " a";
+﻿
+
+// int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {discount}");
+
+
+// int saleAmount = 1001;
+// // int discount = saleAmount > 1000 ? 100 : 50;
+
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+Random test = new Random();
+String result = test.Next(0,2) == 0? "heads" : "tails";
+Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+
+
+
+// string value1 = " a";
 // string value2 = "A ";
 // Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
 
@@ -16,7 +43,7 @@
 // Console.WriteLine(1 >= 1);
 // Console.WriteLine(1 <= 1);
 
-string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox"));
-Console.WriteLine(pangram.Contains("cow"));
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
 
