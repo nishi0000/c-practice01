63d7e6db8daf1b777988c5f9716b88dc3a0db61b
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2024-07-19 08:55:52 +0900

    Add files via upload

--- C#/TestProject/Program.cs
+++ C#/TestProject/Program.cs
@@ -1,13 +1,60 @@
-﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
-int total = 0;
+﻿// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
 
-foreach (int number in numbers)
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+
+switch (product[0])
 {
-    total += number;
-
-    if (number == 42)
-        Console.WriteLine("Set contains 42");
-
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
 }
 
-Console.WriteLine($"Total: {total}");
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+
+
+if (product[2] == "S")
+{
+    size = "Small";
+}
+else if (product[2] == "M")
+{
+    size = "Medium";
+}
+else if (product[2] == "L")
+{
+    size = "Large";
+}
+else
+{
+    size = "One Size Fits All";
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
