b114e98920356d0cefd90d9eb60562d86daa1175
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2024-07-20 22:36:54 +0900

    Add files via upload

--- C#/TestProject/Program.cs
+++ C#/TestProject/Program.cs
@@ -1,60 +1,12 @@
-﻿// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
-
-string[] product = sku.Split('-');
-
-string type = "";
-string color = "";
-string size = "";
-
-
-switch (product[0])
+﻿
+for (int i = 1; i <= 100; i++)
 {
-    case "01":
-        type = "Sweat shirt";
-        break;
-    case "02":
-        type = "T-Shirt";
-        break;
-    case "03":
-        type = "Sweat pants";
-        break;
-    default:
-        type = "Other";
-        break;
+    if (i % 3 == 0 && i % 5 == 0)
+        Console.WriteLine(i + " - FizzBuzz");
+    else if (i % 5 == 0)
+        Console.WriteLine(i + " - Buzz");
+    else if (i % 3 == 0)
+        Console.WriteLine(i + " - Fizz");
+    else
+        Console.WriteLine(i);
 }
-
-switch (product[1])
-{
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color = "Maroon";
-        break;
-    default:
-        color = "White";
-        break;
-}
-
-
-
-if (product[2] == "S")
-{
-    size = "Small";
-}
-else if (product[2] == "M")
-{
-    size = "Medium";
-}
-else if (product[2] == "L")
-{
-    size = "Large";
-}
-else
-{
-    size = "One Size Fits All";
-}
-
-Console.WriteLine($"Product: {size} {color} {type}");
