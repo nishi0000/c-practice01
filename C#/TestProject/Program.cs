ae878fa17a07c86855a012d19451c2ec811d2d18
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2024-07-23 08:51:43 +0900

    Add files via upload

--- C#/TestProject/Program.cs
+++ C#/TestProject/Program.cs
@@ -1,24 +1,33 @@
-﻿
-Random random = new Random();
-int attackPoint = random.Next(1, 11);
+﻿Random random = new Random();
+int attackPoint = 0;
 
 int heroHp = 10;
-int MonsHp = 10;
+int monsterHp = 10;
 
 string trun = "Hero";
 
 do
 {
-    current = random.Next(1, 11);
+    if (trun == "Hero")
+    {
+        attackPoint = random.Next(1, 11);
+        monsterHp -= attackPoint;
+        trun = "Monster";
+        Console.WriteLine($"Monster was damaged and lost {attackPoint} health and now has {monsterHp} health.");
+    }
+    else
+    {
+        attackPoint = random.Next(1, 11);
+        heroHp -= attackPoint;
+        trun = "Hero";
+        Console.WriteLine($"Hero was damaged and lost {attackPoint} health and now has {heroHp} health.");
+    }
+} while (monsterHp > 0 && heroHp > 0);
 
-    if (current >= 8) continue;
 
-    Console.WriteLine(current);
-} while (current != 7);
-
-
-
-
-
+if (heroHp <= 0)
+    Console.WriteLine("Hero loses...");
+else
+    Console.WriteLine("Hero wins!");
 
 
