60c1b4c95b65b004f8b70dcab645d460ca1fdd76
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2024-07-24 08:55:24 +0900

    コード プロジェクト 1 - 整数入力を検証するコードを記述する

--- C#/TestProject/Program.cs
+++ C#/TestProject/Program.cs
@@ -1,33 +1,18 @@
-﻿Random random = new Random();
-int attackPoint = 0;
-
-int heroHp = 10;
-int monsterHp = 10;
-
-string trun = "Hero";
-
+﻿string? readResult;
+int numericValue = 0;
+bool validNumber = false;
+Console.WriteLine("Enter an integer value between 5 and 10");
 do
 {
-    if (trun == "Hero")
-    {
-        attackPoint = random.Next(1, 11);
-        monsterHp -= attackPoint;
-        trun = "Monster";
-        Console.WriteLine($"Monster was damaged and lost {attackPoint} health and now has {monsterHp} health.");
-    }
-    else
-    {
-        attackPoint = random.Next(1, 11);
-        heroHp -= attackPoint;
-        trun = "Hero";
-        Console.WriteLine($"Hero was damaged and lost {attackPoint} health and now has {heroHp} health.");
-    }
-} while (monsterHp > 0 && heroHp > 0);
+    readResult = Console.ReadLine();
+    validNumber = int.TryParse(readResult, out numericValue);
+
+    if (!validNumber)
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    else if (numericValue <= 5 || numericValue >= 10)
+        Console.WriteLine("You entered 2. Please enter a number between 5 and 10.");
 
 
-if (heroHp <= 0)
-    Console.WriteLine("Hero loses...");
-else
-    Console.WriteLine("Hero wins!");
+} while (!validNumber || numericValue <= 5 || numericValue >= 10);
 
-
+Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
