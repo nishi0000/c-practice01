ddfb46ec3a779e266b624a41b4c02cb181ebad1a
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2024-07-25 08:55:26 +0900

    コード プロジェクト 2 - 文字列入力を検証するコードを記述する

--- C#/TestProject/Program.cs
+++ C#/TestProject/Program.cs
@@ -1,18 +1,45 @@
-﻿string? readResult;
-int numericValue = 0;
-bool validNumber = false;
-Console.WriteLine("Enter an integer value between 5 and 10");
+﻿
+
+
+string? readResult;
+string[] role = ["Administrator", "Manager", "User"];
+bool roleMatch = false;
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
 do
 {
     readResult = Console.ReadLine();
-    validNumber = int.TryParse(readResult, out numericValue);
+    readResult = readResult.Trim().ToLower();
 
-    if (!validNumber)
-        Console.WriteLine("Sorry, you entered an invalid number, please try again");
-    else if (numericValue <= 5 || numericValue >= 10)
-        Console.WriteLine("You entered 2. Please enter a number between 5 and 10.");
+    foreach (string str in role)
+    {
+        if (readResult.Equals(str))
+            roleMatch = true;
+    }
+
+    if (roleMatch)
+        Console.WriteLine('The role name that you entered, "{}" is not valid. Enter your role name (Administrator, Manager, or User)');
+
+} while (!roleMatch);
+
+Console.WriteLine($"Your input value ({readResult}) has been accepted.");
 
 
-} while (!validNumber || numericValue <= 5 || numericValue >= 10);
 
-Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+// string? readResult;
+// int numericValue = 0;
+// bool validNumber = false;
+// Console.WriteLine("Enter an integer value between 5 and 10");
+// do
+// {
+//     readResult = Console.ReadLine();
+//     validNumber = int.TryParse(readResult, out numericValue);
+
+//     if (!validNumber)
+//         Console.WriteLine("Sorry, you entered an invalid number, please try again");
+//     else if (numericValue <= 5 || numericValue >= 10)
+//         Console.WriteLine("You entered 2. Please enter a number between 5 and 10.");
+
+
+// } while (!validNumber || numericValue <= 5 || numericValue >= 10);
+
+// Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
