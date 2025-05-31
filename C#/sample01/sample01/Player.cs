e21d35ada3df91a562da951b4b47f227ee9b8bbc
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2025-05-31 14:12:04 +0900

    Add files via upload

---
+++ C#/sample01/sample01/Player.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample01
+{
+    internal class Player
+    {
+        public string PlayerName;
+        public string PlayerClass;
+        public int PlayerHp;
+        public int PlayerMp;
+        public List<String> PlayerItem = new List<string>();
+
+        public Player(string name = "名無し")
+        {
+            PlayerName = name;
+            PlayerClass = "すっぴん";
+        }
+
+        public void SelectClass()
+        {
+            Console.WriteLine("こんにちは、" + PlayerName + "さん。" + "あなたの職業はすっぴんです。");
+            Console.WriteLine("あなたの職業を選んでください。");
+            Console.WriteLine("1.剣士");
+            Console.WriteLine("2.魔法使い");
+            string num = Console.ReadLine();
+
+            if (num == "1")
+            {
+                PlayerClass = "剣士";
+                PlayerHp = 50;
+                PlayerMp = 10;
+                Console.WriteLine("剣士ですね！　HPや攻撃力の高い前衛向きの職業です。頑張ってくださいね！");
+            }
+            else
+            {
+                PlayerHp = 20;
+                PlayerMp = 40;
+                PlayerClass = "魔法使い";
+                Console.WriteLine("魔法使いですね！　MPや魔力の高い後衛向きの職業です。頑張ってくださいね！");
+            }
+        }
+        public string GetPlayerStatus()
+        {
+            return
+$@"=== プレイヤーステータス ===
+名前：{PlayerName}
+職業：{PlayerClass}
+HP ：{PlayerHp}
+MP ：{PlayerMp}
+==========================";
+
+        }
+
+        public void AddItem(string Item)
+        {
+            PlayerItem.Add(Item);
+            Console.WriteLine($"{PlayerName}は{Item}を手に入れた！");
+        }
+
+        public void GetPlayerItem()
+        {
+            Console.WriteLine("======= 持ち物一覧 =======");
+
+            if(PlayerItem.Count == 0)
+            {
+                Console.WriteLine("なし");
+            }
+            {
+                for (int i = 0; i < PlayerItem.Count; i++)
+                {
+                    Console.WriteLine(PlayerItem[i]);
+                }
+            }
+
+            Console.WriteLine("==========================");
+
+        }
+
+    }
+}
---
+++ C#/sample01/sample01/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using sample01;
+
+
+Console.WriteLine("名前を入力してください。");
+string name = Console.ReadLine();
+
+Player player = new Player(name);
+
+player.SelectClass();
+
+Console.WriteLine(player.GetPlayerStatus());
+player.GetPlayerItem();
