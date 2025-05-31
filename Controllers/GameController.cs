64f8ed8d72abae70f38a3b52112f6ec6ad41082f
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2025-05-31 14:45:27 +0900

    Add files via upload

---
+++ Controllers/GameController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RPG.Controllers
+{
+    public class GameController : Controller
+    {
+        private static readonly List<string> Messages = new List<string>
+    {
+        "ようこそ、勇者よ……",
+        "ここは不思議な村だ。",
+        "さあ、冒険に出かけよう！"c
+    };
+
+        private static int messageIndex = 0;
+
+        public IActionResult GetNextMessage()
+        {
+            if (messageIndex < Messages.Count)
+            {
+                return Content(Messages[messageIndex++]);
+            }
+            else
+            {
+                return Content("（おしまい）");
+            }
+        }
+    }
+}
